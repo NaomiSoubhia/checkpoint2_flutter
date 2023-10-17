61e77739535adc685e55096009c02c42d273b3ad
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-16 22:18:58 -0300

    atualizando item

---
+++ lib/pages/item_edicao_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class ItemEdicaoPage extends StatefulWidget {
+  final Map<String, dynamic> item;
+
+  ItemEdicaoPage({required this.item});
+
+  @override
+  _ItemEdicaoPageState createState() => _ItemEdicaoPageState();
+}
+
+class _ItemEdicaoPageState extends State<ItemEdicaoPage> {
+  late TextEditingController textoController;
+
+  @override
+  void initState() {
+    super.initState();
+    textoController = TextEditingController(text: widget.item['texto']);
+  }
+
+  Future<void> atualizarItemNoSupabase(Map<String, dynamic> updatedItem) async {
+    final client = Supabase.instance.client;
+  final response = await client
+        .from('item')
+        .update(updatedItem)
+        .eq('id', updatedItem['id'])
+        .execute();
+
+    if (response.status == 200) {
+      // Item atualizado com sucesso
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Item atualizado com sucesso!')),
+      );
+    } else {
+      // Erro ao atualizar o item
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Erro ao atualizar o item')),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Editar Item'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: textoController,
+              decoration: const InputDecoration(
+                labelText: 'Texto do Item',
+              ),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                final updatedItem = {
+                  'id': widget.item['id'],
+                  'lista': widget.item['lista'],
+                  'texto': textoController.text,
+                };
+
+                // Chame a função para atualizar o item no Supabase
+                atualizarItemNoSupabase(updatedItem);
+              },
+              child: const Text('Salvar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/lista_page.dart
+++ lib/pages/lista_page.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/pages/item_cadastro_page.dart';
+import 'package:expense_tracker/pages/item_edicao_page.dart';
 import 'package:expense_tracker/pages/lista_cadastro_page.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/lista.dart';
@@ -15,37 +16,43 @@ class ListaPage extends StatefulWidget {
 
 class _ListaPageState extends State<ListaPage> {
   TextEditingController itemTextController = TextEditingController();
-  final listaNameController = TextEditingController();
   final client = SupabaseClient(
        'https://lmtfvyxrmihlsharofjd.supabase.co',
-     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxtdGZ2eXhybWlobHNoYXJvZmpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc0ODE3NDEsImV4cCI6MjAxMzA1Nzc0MX0.7jrHphYJAiAmVtSvwXt4w7JqNzOY4L5WgIUNbVhb2ME',
+   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxtdGZ2eXhybWlobHNoYXJvZmpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc0ODE3NDEsImV4cCI6MjAxMzA1Nzc0MX0.7jrHphYJAiAmVtSvwXt4w7JqNzOY4L5WgIUNbVhb2ME',
   
   );
 
-  final _subscription =
-      Supabase.instance.client.from('item').stream(primaryKey: ['id']);
+  final _subscription = Supabase.instance.client.from('item').stream(primaryKey: ['id']);
   List<Map<String, dynamic>> filteredItems = [];
 
-  // Função para excluir um item
   void deleteItem(int itemId) async {
     try {
-      final response = await client
-          .from('item')
-          .delete()
-          .eq('id', itemId)
-          .execute();
+      final response = await client.from('item').delete().eq('id', itemId).execute();
       if (response.status == 200) {
-        // Item excluído com sucesso
         setState(() {
           filteredItems.removeWhere((item) => item['id'] == itemId);
         });
-      } else {
-        
-        print('Excluido com sucesso!');
-        Navigator.pushReplacementNamed(context, '/listas');
       }
     } catch (e) {
       print('Erro ao excluir o item: $e');
+    }
+  }
+
+  void editItem(Map<String, dynamic> item) async {
+    final updatedItem = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => ItemEdicaoPage(item: item),
+      ),
+    );
+
+    if (updatedItem != null) {
+      setState(() {
+        final index = filteredItems.indexWhere((i) => i['id'] == updatedItem['id']);
+        if (index != -1) {
+          filteredItems[index] = updatedItem;
+        }
+      });
     }
   }
 
@@ -55,7 +62,7 @@ class _ListaPageState extends State<ListaPage> {
       appBar: AppBar(
         title: const Text('Itens'),
       ),
-       body: StreamBuilder<List<Map<String, dynamic>>>(
+      body: StreamBuilder<List<Map<String, dynamic>>>(
         stream: _subscription,
         builder: (context, snapshot) {
           if (!snapshot.hasData) {
@@ -72,14 +79,25 @@ class _ListaPageState extends State<ListaPage> {
           return ListView.builder(
             itemCount: filteredItems.length,
             itemBuilder: (context, index) {
+              final item = filteredItems[index];
               return ListTile(
-                title: Text(filteredItems[index]['texto']),
-                trailing: IconButton(
-                  icon: Icon(Icons.delete),
-                  onPressed: () {
-                    // Chame a função para excluir o item
-                    deleteItem(filteredItems[index]['id']);
-                  },
+                title: Text(item['texto']),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () {
+                        deleteItem(item['id']);
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.edit),
+                      onPressed: () {
+                        editItem(item);
+                      },
+                    ),
+                  ],
                 ),
               );
             },
